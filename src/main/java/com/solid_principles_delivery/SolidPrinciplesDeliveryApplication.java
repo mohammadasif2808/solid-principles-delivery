83a34e55dd4bf55a3184bb2cb2279c8de55881ba
mohammadasif2808 <mohammadasif2808@gmail.com>
2025-09-14 13:31:01 +0530

    Initial commit

---
+++ src/main/java/com/solid_principles_delivery/SolidPrinciplesDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.solid_principles_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidPrinciplesDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidPrinciplesDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solid_principles_delivery/SolidPrinciplesDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solid_principles_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidPrinciplesDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
